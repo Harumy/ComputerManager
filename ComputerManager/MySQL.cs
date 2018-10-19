826a20c765011225704688a390fda39d0aa91d0b
harmine <harumy.0321@gmail.com>
2018-10-19 21:02:15 +0900

    MySQLによる他端末の状況取得準備工事

---
+++ ComputerManager/MySQL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ComputerManager
+{
+    class MySQL
+    {
+        private void MySQLConnection()
+        {
+            string ConnectString = "userid=harumy;password=anct270422;database=Tset;Host=harmine.jp";
+            MySqlConnection Connection = new MySqlConnection(ConnectString);
+            Connection.Open();
+        }
+
+
+    }
+}
